89690f23f1236bf2524e3ba48996ba745a5547ef
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-06-11 14:49:53 +0100

    set max and min, created power saving mode

--- jasmine/dist/jasmine-standalone-3.4.0/spec/ThermostatSpec.js
+++ jasmine/dist/jasmine-standalone-3.4.0/spec/ThermostatSpec.js
@@ -5,7 +5,7 @@ describe("Thermostat", function() {
     thermostat = new Thermostat();
   });
 
-  it('starts at 20 degrees', function() {
+  it('is a instance of Thermostat', function() {
     expect(thermostat).toEqual(jasmine.any(Thermostat));
   });
 
@@ -16,5 +16,34 @@ describe("Thermostat", function() {
   it('up can increase temparature by 1', function() {
     thermostat.up();
     expect(thermostat.temperature).toEqual(21);
+  });
+
+  it('down can decrease temparature by 1', function() {
+    thermostat.down();
+    expect(thermostat.temperature).toEqual(19);
+  });
+
+  it('down doesn\'t allow temp to go below 10', function(){
+    thermostat.temperature = 10;
+    thermostat.down();
+    expect(thermostat.temperature).toEqual(10);
+  });
+
+  it('up doesn\'t allow temp to go above 25 in power saving mode', function(){
+    thermostat.temperature = 25;
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(25);
+  });
+
+  it('sets power saving mode to false when turned off', function() {
+    thermostat.powerSaveSwitch();
+    expect(thermostat.powerSavingMode).toEqual(false);
+  });
+
+  it('up doesn\'t allow temp to go above 32 without power saving mode', function(){
+    thermostat.powerSaveSwitch();
+    thermostat.temperature = 32;
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(32);
   });
 });
--- jasmine/dist/jasmine-standalone-3.4.0/src/Thermostat.js
+++ jasmine/dist/jasmine-standalone-3.4.0/src/Thermostat.js
@@ -1,7 +1,30 @@
 function Thermostat () {
   this.temperature = 20;
+  this.minTemp = 10;
+  this.maxTemp = 25;
+  this.powerSavingMode = true;
 };
 
 Thermostat.prototype.up = function() {
-  this.temperature = this.temperature + 1;
+  if (this.temperature < this.maxTemp) {
+    this.temperature = this.temperature + 1;
+  } else {
+    this.temperature = this.maxTemp;
+  }
+};
+
+Thermostat.prototype.down = function() {
+  if (this.temperature > this.minTemp) {
+    this.temperature = this.temperature - 1;
+  }
+};
+
+Thermostat.prototype.powerSaveSwitch = function() {
+  if (this.powerSavingMode === true) {
+    this.powerSavingMode = false;
+    this.maxTemp = 32;
+  } else if (this.powerSavingMode === false) {
+    this.powerSavingMode = true;
+    this.maxTemp = 25;
+  }
 };
