787da6ff98e331d315dc840e804246d28bad48f8
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-06-11 14:01:37 +0100

    test for up method passed

--- jasmine/dist/jasmine-standalone-3.4.0/spec/ThermostatSpec.js
+++ jasmine/dist/jasmine-standalone-3.4.0/spec/ThermostatSpec.js
@@ -12,4 +12,9 @@ describe("Thermostat", function() {
   it('starts at 20 degrees', function() {
     expect(thermostat.temperature).toEqual(20);
   });
+
+  it('up can increase temparature by 1', function() {
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(21);
+  });
 });
--- jasmine/dist/jasmine-standalone-3.4.0/src/Thermostat.js
+++ jasmine/dist/jasmine-standalone-3.4.0/src/Thermostat.js
@@ -2,3 +2,6 @@ function Thermostat () {
   this.temperature = 20;
 };
 
+Thermostat.prototype.up = function() {
+  this.temperature = this.temperature + 1;
+};
